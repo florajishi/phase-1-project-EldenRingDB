05e44bfbfb5f7ee8a604fa6449b94fc23f7fd320
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-03-25 09:21:35 -0500

    implement iterative function for creating table headers and rows

--- index.js
+++ index.js
@@ -12,4 +12,24 @@ document.addEventListener('DOMContentLoaded', (e) => {
         if(element) return element
         throw Error(`${selector} doesn't exist`)
     }
+/*Extract Keys from JSON*/
+const tableCol = []
+for(let i = 0; i < eldenRingData.length; i++){
+    for(const key in eldenRingData[i]){
+        if(tableCol.indexOf(key) === -1){
+            tableCol.push(key)
+        }
+    }
+}
+function createTable(){
+    const table = createElement('table')
+    const tblRow = table.insertRow(-1)
+
+    for(let i = 0; i < tableCol.length; i++){
+        const th = createElement('th');
+        th.innerHTML = tableCol[i]
+        tblRow.appendChild(th)
+    }
+}
+
 })
