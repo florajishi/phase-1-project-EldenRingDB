dadc9629b7a4c98b52963ae0b5be879f1e179bc7
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-03-25 03:45:27 -0500

    Implement getElement and createElement funct for ez query selecting

---
+++ index.js
@@ -0,0 +1,12 @@
+function getElement(selector){
+    const element = document.querySelector(selector)
+
+    if(element) return element
+    throw Error(`${selector} doesn't exist`)
+}
+function createElement(selector){
+    const element = document.createElement(selector)
+
+    if(element) return element
+    throw Error(`${selector} doesn't exist`)
+}
