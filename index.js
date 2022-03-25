35f37023dedcd960dd0b8a0bdce6e97439a4727d
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-03-25 04:25:19 -0500

    Add DOM load event listener connect js to html

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-function getElement(selector){
-    const element = document.querySelector(selector)
+document.addEventListener('DOMContentLoaded', (e) => {
+    
+    function getElement(selector){
+        const element = document.querySelector(selector)
 
-    if(element) return element
-    throw Error(`${selector} doesn't exist`)
-}
-function createElement(selector){
-    const element = document.createElement(selector)
+        if(element) return element
+        throw Error(`${selector} doesn't exist`)
+    }
+    function createElement(selector){
+        const element = document.createElement(selector)
 
-    if(element) return element
-    throw Error(`${selector} doesn't exist`)
-}
+        if(element) return element
+        throw Error(`${selector} doesn't exist`)
+    }
+})
