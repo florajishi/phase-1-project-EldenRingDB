e68340de35307ff7a9ca99170fa086f7d18f1643
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-03-29 09:53:15 -0500

    build dropdown menu funct implemented, wip function for selecting options in menu

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', e =>{
+    const URL_PREFIX = 'https://eldenring.fanapis.com/api/'
     /* get and create Element functions*/
     function getElement(selector){
         const element = document.querySelector(selector)
@@ -13,59 +14,62 @@ document.addEventListener('DOMContentLoaded', e =>{
         if(element) return element
         throw Error(`${selector} doesn't exist`)
     }
-    const fetchCategory = selectedCategory => {
-        fetch(`${URL_PREFIX}${selectedCategory}`)
-        .then(async resp => {
-            if(!resp.ok){
-                throw Error('ERROR')
-            }
-            const results = await resp.json()
-            const allData = results.data.map(item => (item))
-            console.log(allData)
-        })
-    }
+    // function fetchCategory(selectedCategory){
+    //     fetch(`${URL_PREFIX}${selectedCategory}`)
+    //     .then(async resp => {
+    //         if(!resp.ok){
+    //             throw Error('ERROR')
+    //         }
+    //         const results = await resp.json()
+    //         const allData = results.data.map(item => (item))
+    //         console.log(allData)
+    //     })
+    // }
     const select = getElement('#category-select')
-    const buildDropdwnMenu = () =>{
-        const categories = new Array(
-            'ammos', 'armors', 'ashes', 'bosses', 'creatures', 'incantations', 'items', 'locations', 'npcs', 'shields', 'sorceries', 'spirits', 'talismans', 'weapons'
-            )
+    const categories = ['ammos', 'armors', 'ashes', 'bosses', 'creatures', 'incantations', 'items', 'locations', 'npcs', 'shields', 'sorceries', 'spirits', 'talismans', 'weapons']
+
+    select.addEventListener('click', buildDropdwnMenu, {once: true})
+
+    function buildDropdwnMenu(){
         for(let i = 0; i < categories.length; i++){
-            let option = categories[i]
-            const optElem = createElement('option')
-            optElem.textContent = option
-            optElem.value = option
-            select.appendChild(optElem)
+            let opt = categories[i]
+            let selectOption = createElement("option")
+            selectOption.textContent = opt
+            selectOption.value = opt
+            select.appendChild(selectOption)
+            console.log(selectOption.textContent)
         }
-    }
-    const categorySelection = categories => {
+}        
+
+    function categorySelection(){
+        let optionTxt = select.options
         for(let i = 0; i < select.length; i++){
-            if(select.options[i].value === categories[i]){
+            if(optionTxt[i].value == categories[i]){
                 select.selectedIndex = i
+                console.log(optionTxt[i])
             }
         }
+        // let categorySel = select.options[select.selectedIndex].text
+        // fetchCategory(categorySel)
     }
-    function fetchJson(urls){
-        fetch(urls)
-        .then(async resp => {
-            if(!resp.ok){
-                throw Error('ERROR')
-            }
-            const results = await resp.json()
-            const allData = results.data.map(item => (item))
-            // console.log(allData)
-            categorizeData(allData)
-        })
-    }
-    function categorizeData(data){
-        for(let i = 0; i < data.length; i++){
-            console.log(data[i])
+
+    // const categorySelection = categories => {
+    //     for(let i = 0; i < select.length; i++){
+    //         if(select.options[i].value === categories[i]){
+    //             select.selectedIndex = i
+    //         }
+    //     }
+    // }
+    // function categorizeData(data){
+    //     for(let i = 0; i < data.length; i++){
+    //         console.log(data[i])
             
-        }
-    }
-    function getKeyNames(data){
-        let keyNames = []
-        Object.keys(data[0].forEach(key => keyNames.push(key)))
-        console.log(keyNames)
-    }
+    //     }
+    // }
+    // function getKeyNames(data){
+    //     let keyNames = []
+    //     Object.keys(data[0].forEach(key => keyNames.push(key)))
+    //     console.log(keyNames)
+    // }
 
 })
